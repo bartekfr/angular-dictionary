c9128d5611b6ebeda1181efd2496c6d24a99d0f7
bartek <bartekfr@wp.pl>
2013-10-20 22:01:24 +0200

    custom REST

--- js/dictionary.js
+++ js/dictionary.js
@@ -1,14 +1,22 @@
-var dictionaryApp = angular.module('dictionaryApp', ["ngResource"]);
+var dictionaryApp = angular.module('dictionaryApp', ["entriesResource"]);
  
-dictionaryApp.controller('entriesController', function entriesController($scope, $http) {
+dictionaryApp.constant('MONGOLAB_CONFIG', {
+	DB_NAME: 'dict',
+	API_KEY: 'DMXpO9AqPJbloiV0hGkgnuRD45pyGGSx'
+})
+.factory('Entries', function(entriesResource){
+	return entriesResource('EntriesCollection')
+
+})
+.controller('entriesController', function entriesController($scope, Entries) {
 	$scope.entries = [];
-	var responsePromise = $http.get('https://api.mongolab.com/api/1/databases/dict/collections/EntriesCollection',{
+	/*var responsePromise = $http.get('https://api.mongolab.com/api/1/databases/dict/collections/EntriesCollection',{
 		params:{
 			apiKey: 'DMXpO9AqPJbloiV0hGkgnuRD45pyGGSx'
 		}
-	});
-	responsePromise.then(function(resp) {
-		$.each(resp.data, function(i, ob) {
+	});*/
+	Entries.query().then(function(resp) {
+		$.each(resp, function(i, ob) {
 			$scope.entries.push({
 				english: ob.englishWord,
 				polish: ob.translatedWord
