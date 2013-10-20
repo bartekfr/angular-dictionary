86611c62b6ec4897a29f2f8770160339d123f0c0
bartek <bartekfr@wp.pl>
2013-10-20 13:18:15 +0200

    getting data with promise API

--- js/dictionary.js
+++ js/dictionary.js
@@ -2,17 +2,20 @@ var dictionaryApp = angular.module('dictionaryApp', []);
  
 dictionaryApp.controller('entriesController', function entriesController($scope, $http) {
 	$scope.entries = [];
-	$http.get('https://api.mongolab.com/api/1/databases/dict/collections/EntriesCollection',{
+	var responsePromise = $http.get('https://api.mongolab.com/api/1/databases/dict/collections/EntriesCollection',{
 		params:{
 			apiKey: 'DMXpO9AqPJbloiV0hGkgnuRD45pyGGSx'
 		}
-	}).success(function(resp) {
-		$.each(resp, function(i, ob) {
+	});
+	responsePromise.then(function(resp) {
+		$.each(resp.data, function(i, ob) {
 			$scope.entries.push({
 				english: ob.englishWord,
 				polish: ob.translatedWord
 			})
-		})
+		});
+	}, function(resp) {
+		console.log('there was problem with database connection', resp)
 	});
 
 });
