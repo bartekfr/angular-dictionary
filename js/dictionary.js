2a32edf8b12c6f588ece1bd26a7a7025b1d2082e
bartek <bartekfr@wp.pl>
2013-10-25 20:05:59 +0200

    simple pagination

--- js/dictionary.js
+++ js/dictionary.js
@@ -8,8 +8,17 @@ dictionaryApp.constant('MONGOLAB_CONFIG', {
 	return entriesResource('EntriesCollection')
 
 })
+.filter('pagination', function() {
+	return function(input, page, size) {
+		var start = page * size;
+		return input.slice(start, start + size)
+	}
+})
 .controller('entriesController', function entriesController($scope, Entries) {
 	$scope.entries = [];
+	$scope.page = 0;
+	$scope.pageSize = 3;
+	$scope.dataLength = 0;
 	/*var responsePromise = $http.get('https://api.mongolab.com/api/1/databases/dict/collections/EntriesCollection',{
 		params:{
 			apiKey: 'DMXpO9AqPJbloiV0hGkgnuRD45pyGGSx'
@@ -22,8 +31,15 @@ dictionaryApp.constant('MONGOLAB_CONFIG', {
 				polish: ob.translatedWord
 			})
 		});
+		$scope.dataLength = $scope.entries.length;
+		console.log($scope.dataLength );
 	}, function(resp) {
 		console.log('there was problem with database connection', resp)
 	});
-
+	$scope.setPage = function(page) {
+		if(page >= 0 && page < $scope.dataLength / $scope.pageSize) {
+			$scope.page = page;
+		}
+		return false;
+	}
 });
